f313b115af0338f60a12c7d43e0f2f9f5d5c2b09
Snapdragon845GMS <79841935+Snapdragon845GMS@users.noreply.github.com>
2022-11-30 10:04:37 +0100

    Update Form1.cs

--- Ejercicio 12/Ejercicio12/Form1.cs
+++ Ejercicio 12/Ejercicio12/Form1.cs
@@ -19,16 +19,16 @@ namespace Ejercicio12
 
         private void Bcalcular_Click(object sender, EventArgs e)
         {
-            //sumamos los tres productos
+            
             int num1 = Int32.Parse(B1.Text);
             int num2 = Int32.Parse(B2.Text);
             int num3 = Int32.Parse(B3.Text);
 
-            //el total a pagar sin iva
+           
             int res = num1 + num2 + num3;
-            //con iva
+            
             double res_iva = (res * 0.21) + res;
-            //lo ostramos en su Box adecuado
+            
             Bsi.Text = res.ToString();
             Bci.Text = res_iva.ToString();
 
