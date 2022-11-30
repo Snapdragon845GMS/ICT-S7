8432922b77dbe1fba2c098968b106f9ffc43d5bc
Snapdragon845GMS <79841935+Snapdragon845GMS@users.noreply.github.com>
2022-11-30 09:54:34 +0100

    Add files via upload

---
+++ Ejercicio 8/Ejercicio8/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Ejercicio8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tnum1 = new System.Windows.Forms.TextBox();
+            this.Tnum3 = new System.Windows.Forms.TextBox();
+            this.Tnum2 = new System.Windows.Forms.TextBox();
+            this.Boperar = new System.Windows.Forms.Button();
+            this.Lmultiplicacion = new System.Windows.Forms.Label();
+            this.Lsuma = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Tnum1
+            // 
+            this.Tnum1.Location = new System.Drawing.Point(119, 50);
+            this.Tnum1.Name = "Tnum1";
+            this.Tnum1.Size = new System.Drawing.Size(100, 20);
+            this.Tnum1.TabIndex = 0;
+            // 
+            // Tnum3
+            // 
+            this.Tnum3.Location = new System.Drawing.Point(119, 280);
+            this.Tnum3.Name = "Tnum3";
+            this.Tnum3.Size = new System.Drawing.Size(100, 20);
+            this.Tnum3.TabIndex = 1;
+            // 
+            // Tnum2
+            // 
+            this.Tnum2.Location = new System.Drawing.Point(119, 168);
+            this.Tnum2.Name = "Tnum2";
+            this.Tnum2.Size = new System.Drawing.Size(100, 20);
+            this.Tnum2.TabIndex = 2;
+            // 
+            // Boperar
+            // 
+            this.Boperar.Location = new System.Drawing.Point(377, 50);
+            this.Boperar.Name = "Boperar";
+            this.Boperar.Size = new System.Drawing.Size(147, 138);
+            this.Boperar.TabIndex = 3;
+            this.Boperar.Text = "operar";
+            this.Boperar.UseVisualStyleBackColor = true;
+            this.Boperar.Click += new System.EventHandler(this.Boperar_Click);
+            // 
+            // Lmultiplicacion
+            // 
+            this.Lmultiplicacion.AutoSize = true;
+            this.Lmultiplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Lmultiplicacion.Location = new System.Drawing.Point(432, 283);
+            this.Lmultiplicacion.Name = "Lmultiplicacion";
+            this.Lmultiplicacion.Size = new System.Drawing.Size(60, 24);
+            this.Lmultiplicacion.TabIndex = 4;
+            this.Lmultiplicacion.Text = "          ";
+            // 
+            // Lsuma
+            // 
+            this.Lsuma.AutoSize = true;
+            this.Lsuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.Lsuma.Location = new System.Drawing.Point(432, 219);
+            this.Lsuma.Name = "Lsuma";
+            this.Lsuma.Size = new System.Drawing.Size(60, 24);
+            this.Lsuma.TabIndex = 5;
+            this.Lsuma.Text = "         ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 382);
+            this.Controls.Add(this.Lsuma);
+            this.Controls.Add(this.Lmultiplicacion);
+            this.Controls.Add(this.Boperar);
+            this.Controls.Add(this.Tnum2);
+            this.Controls.Add(this.Tnum3);
+            this.Controls.Add(this.Tnum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Tnum1;
+        private System.Windows.Forms.TextBox Tnum3;
+        private System.Windows.Forms.TextBox Tnum2;
+        private System.Windows.Forms.Button Boperar;
+        private System.Windows.Forms.Label Lmultiplicacion;
+        private System.Windows.Forms.Label Lsuma;
+    }
+}
+
---
+++ Ejercicio 8/Ejercicio8/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Boperar_Click(object sender, EventArgs e)
+        {
+            //leemos las variables iniciales
+            int num1 = Int32.Parse(Tnum1.Text);
+            int num2 = Int32.Parse(Tnum2.Text);
+            int num3 = Int32.Parse(Tnum3.Text);
+
+            //operamos con las variables
+            int sum = num1 + num2 + num3;
+            int mult = num1 * num2 * num3;
+
+            //mostramos cada operacion en su label
+            Lsuma.Text = sum.ToString();
+            Lmultiplicacion.Text = mult.ToString();
+        
+        }
+    }
+}
