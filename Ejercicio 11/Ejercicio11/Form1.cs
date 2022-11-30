bfd267a1c3ad3db84a0b913fb06fb771e3da7f18
Snapdragon845GMS <79841935+Snapdragon845GMS@users.noreply.github.com>
2022-11-30 10:02:43 +0100

    Update Form1.cs

--- Ejercicio 11/Ejercicio11/Form1.cs
+++ Ejercicio 11/Ejercicio11/Form1.cs
@@ -19,13 +19,13 @@ namespace Ejercicio11
 
         private void Bcalcular_Click(object sender, EventArgs e)
         {
-            //leemos variables iniciales
+            
             int num1 = Int32.Parse(Tinicial.Text);
             int num2 = Int32.Parse(Tporcentaje.Text);
-            //operamos con las variables
+            
             int por = (num1 * num2) / 100;
             int res = por + num1;
-            //mostramos los resultado en el label
+            
             Lfinal.Text = res.ToString();
         }
     }
