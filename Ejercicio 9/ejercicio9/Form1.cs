97b918c480102f5d8b587186881546d90063b503
Snapdragon845GMS <79841935+Snapdragon845GMS@users.noreply.github.com>
2022-11-30 10:01:17 +0100

    Update Form1.cs

--- Ejercicio 9/ejercicio9/Form1.cs
+++ Ejercicio 9/ejercicio9/Form1.cs
@@ -19,65 +19,64 @@ namespace ejercicio9
 
         private void sum_Click(object sender, EventArgs e)
         {
-            //leemos las variables iniciales
+            
             int num1 = Int32.Parse(Tnum1.Text);
             int num2 = Int32.Parse(Tnum2.Text);
 
-            //sumamos los numeros
+            
             int res1 = num1 + num2;
 
-            //mostramos el resultado por pantalla
+           
             Bres.Text = res1.ToString();
         }
         private void restar_Click(object sender, EventArgs e)
         {
-            //leemos las variables iniciales
+            
             int num1 = Int32.Parse(Tnum1.Text);
             int num2 = Int32.Parse(Tnum2.Text);
 
-            //sumamos los numeros
+            
             int res2 = num1 - num2;
 
-            //mostramos el resultado por pantalla
+            
             Bres.Text = res2.ToString();
         }
 
         private void mult_Click_1(object sender, EventArgs e)
         {
-            //leemos las variables iniciales
+            
             int num1 = Int32.Parse(Tnum1.Text);
             int num2 = Int32.Parse(Tnum2.Text);
 
-            //sumamos los numeros
+            
             int res3 = num1 * num2;
 
-            //mostramos el resultado por pantalla
+            
             Bres.Text = res3.ToString();
         }
 
         private void div_Click_1(object sender, EventArgs e)
         {
-            //leemos las variables iniciales
+            
             int num1 = Int32.Parse(Tnum1.Text);
             int num2 = Int32.Parse(Tnum2.Text);
 
-            //sumamos los numeros
             int res4 = num1 / num2;
 
-            //mostramos el resultado por pantalla
+            
             Bres.Text = res4.ToString();
         }
 
         private void resto_Click_1(object sender, EventArgs e)
         {
-            //leemos las variables iniciales
+            
             int num1 = Int32.Parse(Tnum1.Text);
             int num2 = Int32.Parse(Tnum2.Text);
 
-            //sumamos los numeros
+            
             int res5 = num1 % num2;
 
-            //mostramos el resultado por pantalla
+            
             Bres.Text = res5.ToString();
         }
     }
