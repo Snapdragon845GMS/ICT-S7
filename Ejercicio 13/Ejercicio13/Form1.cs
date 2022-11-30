31f57931682b37b8179a62973bd4687f9cfaaf53
Snapdragon845GMS <79841935+Snapdragon845GMS@users.noreply.github.com>
2022-11-30 09:58:56 +0100

    Add files via upload

---
+++ Ejercicio 13/Ejercicio13/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(textBox1.Text);//horas normales
+            int num2 = Int32.Parse(textBox2.Text);//horas extras
+            int num3 = Int32.Parse(textBox3.Text);//paga por hora
+
+            //calculamos horas totales, suponemos 30 dias en un mes y por lo tanto 22 dias lectivos
+            int horas_totales = (num1 * num3 * 22) + (num2 * 2 * num3 * 22);
+            double res = horas_totales * 0.18;
+
+            //mostramos el resultado
+            label3.Text = res.ToString();
+
+        }
+    }
+}
---
+++ Ejercicio 14/Ejercicio14/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox1.Text);
+
+            bool res = num1 != num2;
+            label3.Text = res.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox1.Text);
+
+            bool res = num1 == num2;
+            label3.Text = res.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox1.Text);
+
+            bool res = num1 < num2;
+            label3.Text = res.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox1.Text);
+
+            bool res = num1 > num2;
+            label3.Text = res.ToString();
+        }
+    }
+}
