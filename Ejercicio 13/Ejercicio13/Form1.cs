7a866213f8a55a6be29116e4d527bc545fe583c7
Snapdragon845GMS <79841935+Snapdragon845GMS@users.noreply.github.com>
2022-11-30 10:05:21 +0100

    Update Form1.cs

--- Ejercicio 13/Ejercicio13/Form1.cs
+++ Ejercicio 13/Ejercicio13/Form1.cs
@@ -19,15 +19,14 @@ namespace Ejercicio13
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int num1 = Int32.Parse(textBox1.Text);//horas normales
-            int num2 = Int32.Parse(textBox2.Text);//horas extras
-            int num3 = Int32.Parse(textBox3.Text);//paga por hora
-
-            //calculamos horas totales, suponemos 30 dias en un mes y por lo tanto 22 dias lectivos
+            int num1 = Int32.Parse(textBox1.Text);
+            int num2 = Int32.Parse(textBox2.Text);
+            int num3 = Int32.Parse(textBox3.Text);
+            
             int horas_totales = (num1 * num3 * 22) + (num2 * 2 * num3 * 22);
             double res = horas_totales * 0.18;
 
-            //mostramos el resultado
+            
             label3.Text = res.ToString();
 
         }
